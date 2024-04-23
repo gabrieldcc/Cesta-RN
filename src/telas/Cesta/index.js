bb055f93ece2f48adaa8ceb68aa20fa7b2c22a67
Gabriel Castro <gabrieldcc@gmail.com>
2024-04-23 12:36:26 -0300

    feature: flat list

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,25 +1,39 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, View, ScrollView } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 
 import Topo from '../componentes/Topo'
 import Detalhes from "../componentes/Detalhes";
-import Itens from "../componentes/Itens";
+import Item from "../componentes/Item";
 
 export default function Cesta({ topo, detalhes, itens }) {
 
-    return <ScrollView>
-        <SafeAreaView>
-            <Topo {...topo} />
-            <View style={estilos.cesta}>
-                <Detalhes {...detalhes} />
-                <Itens {...itens} />
-            </View>
-        </SafeAreaView>
-    </ScrollView>
+    return <>
+        <FlatList
+            data={itens.lista}
+            renderItem={Item}
+            keyExtractor={({ nome }) => nome}
+            ListHeaderComponent={() => {
+                return <>
+                    <Topo {...topo} />
+                    <View style={estilos.cesta}>
+                        <Detalhes {...detalhes} />
+                    </View>
+                </>
+            }}
+        />
+    </>
 }
 
 const estilos = StyleSheet.create({
+    titulo: {
+        color: "#464646",
+        fontWeight: "bold",
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32,
 
+    },
     cesta: {
         paddingVertical: 8,
         paddingHorizontal: 16,
