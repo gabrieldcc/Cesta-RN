97f5457fb60f76ccd479925c9bf59d8d5ca8157b
Gabriel Castro <gabrieldcc@gmail.com>
2024-04-22 21:03:46 -0300

    tomate image added

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,4 +1,5 @@
 import logo from '../../assets/logo.png';
+import tomate from '../../assets/frutas/Tomate.png'
 
 const cesta = {
     topo: {
@@ -11,6 +12,16 @@ const cesta = {
         descricao: "Uma cesta com produtos selecionados cuidadosamente direto para sua cozinha",
         preco: "R$ 40,00",
         botao: "Comprar",
+    },
+    itens: {
+        titulo: "Itens da cesta",
+        lista: [
+            {
+                nome: "Tomate",
+                imagem: tomate,
+
+            }
+        ]
     }
 }
 
